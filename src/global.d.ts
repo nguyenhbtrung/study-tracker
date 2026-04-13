2293d9a3a0eef7e8ec01e4eb3fc5bb377dd554f1
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2026-04-13 20:00:42 +0700

    update

---
+++ src/global.d.ts
@@ -0,0 +1,12 @@
+export {};
+
+declare global {
+  interface Window {
+    api: {
+      getStats: () => Promise<{
+        today: number;
+        total: number;
+      }>;
+    };
+  }
+}
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,7 +26,26 @@
  * ```
  */
 
-import "./index.css";
+import './index.css';
+
+function format(ms: number): string {
+  const sec = Math.floor(ms / 1000);
+  const h = Math.floor(sec / 3600);
+  const m = Math.floor((sec % 3600) / 60);
+  const s = sec % 60;
+
+  return `${h}h ${m}m ${s}s`;
+}
+
+async function update() {
+  const stats = await (window as any).api.getStats();
+
+  document.getElementById('today')!.innerText = format(stats.today);
+  document.getElementById('total')!.innerText = format(stats.total);
+}
+
+setInterval(update, 1000);
+update();
 
 console.log(
   '👋 This message is being logged by "renderer.js", included via webpack',
